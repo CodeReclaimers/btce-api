b10249b8deaf5e4876c46a737b5ed562f5df1ff3
LolUsernameTaken <g4nd41ph@gmail.com>
2013-04-26 08:42:24 -0300

    Update common.py

--- btceapi/common.py
+++ btceapi/common.py
@@ -72,7 +72,7 @@ def validatePair(pair):
         raise Exception("Unrecognized pair: %r" % pair)
         
 def formatCurrency(value, maxdigits):
-    s = round(s, maxdigits)
+    s = round(value, maxdigits)
     s = "%.8f" % value
     s = s[:s.index(".")+maxdigits+1]
     
