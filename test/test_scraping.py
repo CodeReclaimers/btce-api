2de7d90b067f9b32bfc571adbaea50afd60ba948
CodeReclaimers <github@codereclaimers.com>
2017-04-08 18:50:03 -0400

    Added smoke test for scraping.

---
+++ test/test_scraping.py
@@ -0,0 +1,15 @@
+import decimal
+import unittest
+import btceapi
+
+
+class TestScraping(unittest.TestCase):
+    def test_scrape_main_page(self):
+        mainPage = btceapi.scrapeMainPage()
+        for message in mainPage.messages:
+            msgId, user, time, text = message
+            print "%s %s: %s" % (time, user, text)
+
+
+if __name__ == '__main__':
+    unittest.main()
