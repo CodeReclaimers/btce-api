4b8fa0fced7e8a8f785430164b577dcaf3f98b00
CodeReclaimers <github@codereclaimers.com>
2017-04-08 19:44:37 -0400

    Code cleanup.

--- test/test_public.py
+++ test/test_public.py
@@ -1,36 +1,36 @@
-import decimal
 import unittest
+
+from btceapi.common import BTCEConnection
 from btceapi.public import *
-import btceapi
 
 
 class TestPublic(unittest.TestCase):
     def test_getTicker(self):
-        connection = btceapi.BTCEConnection()
-        info = btceapi.APIInfo(connection)
+        connection = BTCEConnection()
+        info = APIInfo(connection)
         for pair in info.pair_names:
-            btceapi.getTicker(pair, connection, info)
-            btceapi.getTicker(pair, connection)
-            btceapi.getTicker(pair, info=info)
-            btceapi.getTicker(pair)
+            getTicker(pair, connection, info)
+            getTicker(pair, connection)
+            getTicker(pair, info=info)
+            getTicker(pair)
 
     def test_getHistory(self):
-        connection = btceapi.BTCEConnection()
-        info = btceapi.APIInfo(connection)
+        connection = BTCEConnection()
+        info = APIInfo(connection)
         for pair in info.pair_names:
-            btceapi.getTradeHistory(pair, connection, info)
-            btceapi.getTradeHistory(pair, connection)
-            btceapi.getTradeHistory(pair, info=info)
-            btceapi.getTradeHistory(pair)
+            getTradeHistory(pair, connection, info)
+            getTradeHistory(pair, connection)
+            getTradeHistory(pair, info=info)
+            getTradeHistory(pair)
 
     def test_getDepth(self):
-        connection = btceapi.BTCEConnection()
-        info = btceapi.APIInfo(connection)
+        connection = BTCEConnection()
+        info = APIInfo(connection)
         for pair in info.pair_names:
-            btceapi.getDepth(pair, connection, info)
-            btceapi.getDepth(pair, connection)
-            btceapi.getDepth(pair, info=info)
-            btceapi.getDepth(pair)
+            getDepth(pair, connection, info)
+            getDepth(pair, connection)
+            getDepth(pair, info=info)
+            getDepth(pair)
 
     def test_constructTrade(self):
         d = {"pair": "btc_usd",
